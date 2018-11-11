553b3e549c6c60959082929b31005309f3a236e8
Aventhor <vaster399@gmail.com>
2018-11-12 00:06:57 +0300

    added models

--- FlyBowtie/app/admin.py
+++ FlyBowtie/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Person, Position
 
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    model = Person
+
+class PositonAdmin(admin.ModelAdmin):
+    model = Position
+
+admin.site.register(Person)
+admin.site.register(Position)
--- FlyBowtie/app/models.py
+++ FlyBowtie/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length = 50, verbose_name="Имя")
+    last_name = models.CharField(max_length = 50, verbose_name="Фамилия")
+    position = models.ForeignKey('Position', on_delete=models.CASCADE, verbose_name="Должность")
+    class Meta(object):
+        verbose_name_plural = "Люди"
+        verbose_name = "человек"
+
+    def __str__(self):
+        return '{0} {1} | {2}'.format(self.first_name, self.last_name, self.position)
+
+class Position(models.Model):
+    name = models.CharField(max_length=50, unique=True ,verbose_name="Должность")
+    class Meta(object):
+        verbose_name_plural = "Должности"
+        verbose_name = "должность"
+    def __str__(self):
+        return self.name
